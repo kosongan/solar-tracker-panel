794761ad49e28d64c8853bf7424110b0459e2bb7
kosongan <68933588+kosongan@users.noreply.github.com>
2020-08-26 16:46:46 +0700

    Update solarpanel.ino

--- solarpanel.ino
+++ solarpanel.ino
@@ -18,34 +18,34 @@ void loop()
 
 {
  
-    if (waktu.mon=1 && waktu.hour=6 && waktu.min=0)        //Januari
-       { 
+    if (waktu.mon=1 && waktu.hour=6 && waktu.min=0)               // Januari
+       {  
        delay(150);
        servo.write(8);
-       for(derajat=8;derajat <= 175; derajat+=1.2)        // sudut jam 6.00, 8 derajat, bergerak 1.2 derajat setiap 5 menit
+       for(derajat=8;derajat <= 175; derajat+=1.2)                // sudut jam 6.00, 8 derajat, bergerak 1.2 derajat setiap 5 menit
        {
        servo.write(derajat);                            
        delay(300000);                              
        }
-       for(derajat=175;derajat>=8;derajat-=5)            // servo kembali ke posisi 8 derajat
+       for(derajat=175;derajat>=8;derajat-=5)                    // servo kembali ke posisi 8 derajat
        {
        servo.write(derajat);                           
        delay(1000);
        }
-       delay(43200000);                                  // menunggu 12 jam untuk kembali bergerak 
+       delay(43200000);                                          // menunggu 12 jam untuk kembali bergerak 
        }
 
-else if (waktu.mon=2 && waktu.hour=6 && waktu.min=0)    // Februari
+else if (waktu.mon=2 && waktu.hour=6 && waktu.min=0)             // Februari
         {
         delay(150);
         servo.write(2);
        
-       for(derajat=2;derajat <= 176; derajat+=1.3)     // sudut awal jam 6.00, 2 derajat, bergerak 1 derajat setiap 4 menit
+       for(derajat=2;derajat <= 176; derajat+=1.3)               // sudut awal jam 6.00, 2 derajat, bergerak 1 derajat setiap 4 menit
        {
        servo.write(derajat);                  
        delay(240000);
        }
-       for(derajat=176;derajat>=2;derajat-=5)          // servo kembali ke posisi 2 derajat
+       for(derajat=176;derajat>=2;derajat-=5)                    // servo kembali ke posisi 2 derajat
        {
        servo.write(derajat);
        delay(1000);
@@ -54,28 +54,28 @@ else if (waktu.mon=2 && waktu.hour=6 && waktu.min=0)    // Februari
        }
   
 
-else if (waktu.mon=3 && waktu.hour=6 && waktu.min=0)   // Maret
+else if (waktu.mon=3 && waktu.hour=6 && waktu.min=0)             // Maret
        {
        (delay(150);
        servo.write(2); 
        
-       for(derajat=2;derajat <= 176; derajat+=1)      // sudut awal 6.00, 2 derajat, bergerak 1 derajat setiap 4 menit
+       for(derajat=2;derajat <= 176; derajat+=1)                // sudut awal 6.00, 2 derajat, bergerak 1 derajat setiap 4 menit
        {
        servo.write(derajat);
        delay(240000);
        }      
-       for(derajat=176;derajat>=2;derajat-=5){        // servo kembali ke posisi 2 derajat
+       for(derajat=176;derajat>=2;derajat-=5){                  // servo kembali ke posisi 2 derajat
        servo.write(derajat);
        delay(1000);
        }
       delay(43200000);
        }
 
-else if  (waktu.mon=4 && waktu.hour=6 && waktu.min=0) // April
+else if  (waktu.mon=4 && waktu.hour=6 && waktu.min=0)           // April
        {
        delay(150);
        servo.write(2);
-       for(derajat=2;derajat <= 176; derajat+=1.3)    // sudut awal 6.00, 2 derajat, bergerak 1 derajat setiap 4 menit
+       for(derajat=2;derajat <= 176; derajat+=1.3)              // sudut awal 6.00, 2 derajat, bergerak 1 derajat setiap 4 menit
        {
        servo.write(derajat);                 
        delay(240000);
@@ -88,55 +88,90 @@ else if  (waktu.mon=4 && waktu.hour=6 && waktu.min=0) // April
        delay(43200000);
        }  
 
-else if (waktu.mon=5 && waktu.hour=6 && waktu.min=0)
+  
+else if  (waktu.mon=5 && waktu.hour=6 && waktu.min=0)          // Mei
        {
        delay(150);
        servo.write(2);
-       for(derajat=2;derajat <= 176; derajat+=1.3){
-
+       for(derajat=2;derajat <= 176; derajat+=1.3)             // sudut awal 6.00, 2 derajat, bergerak 1 derajat setiap 4 menit
+       {
+       servo.write(derajat);                 
+       delay(240000);
+       }
+       for(derajat=176;derajat>=2;derajat-=5)
+       {
        servo.write(derajat);
-       delay(15);                    // waits 15ms for the servo to reach the position
-       delay(300000);}
-       for(derajat=176;derajat>=0;derajat-=5){
-       servo.write(derajat);
-  
-  delay(15);                       // waits 15ms for the servo to reach the position
-
-  delay(500);}
-  delay(43200000);
-  }
-
-  else if (waktu.mon=6) { (waktu.hour=6 && waktu.min=0)
+       delay(500);
+       }
+       delay(43200000);
+       }  
+        
+  else if  (waktu.mon=6 && waktu.hour=6 && waktu.min=0)        // Juni
+       {
        delay(150);
        servo.write(0);
-       for(derajat=0;derajat <= 176; derajat+=1.3){
-
+       for(derajat=0;derajat <= 176; derajat+=1.5)            // sudut awal 6.00, 0 derajat, bergerak 1.5 derajat setiap 5 menit
+       {
+       servo.write(derajat);                 
+       delay(300000);
+       }
+       for(derajat=176;derajat>=0;derajat-=5)
+       {
        servo.write(derajat);
-       delay(15);                    // waits 15ms for the servo to reach the position
-       delay(300000);}
-       for(derajat=176;derajat>=0;derajat-=5){
-       servo.write(derajat);
-  
-  delay(15);                       // waits 15ms for the servo to reach the position
-
-  delay(500);}
-  delay(43200000);
-  }
-
-   else if (waktu.mon=7) { (waktu.hour=6 && waktu.min=0)
+       delay(500);
+       }
+       delay(43200000);
+       }  
+        
+   else if  (waktu.mon=7 && waktu.hour=6 && waktu.min=0)      // Juli
+       {
        delay(150);
        servo.write(0);
-       for(derajat=0;derajat <= 176; derajat+=1.3){
-
+       for(derajat=0;derajat <= 176; derajat+=1.4)            // sudut awal 6.00, 0 derajat, bergerak 1.4 derajat setiap 5 menit
+       {
+       servo.write(derajat);                 
+       delay(300000);
+       }
+       for(derajat=176;derajat>=0;derajat-=5)
+       {
        servo.write(derajat);
-       delay(300000);}
-       for(derajat=176;derajat>=0;derajat-=5){
+       delay(500);
+       }
+       delay(43200000);
+       }  
+        
+   else if  (waktu.mon=8 && waktu.hour=6 && waktu.min=0)       // Agustus
+       {
+       delay(150);
+       servo.write(1);
+       for(derajat=1;derajat <= 176; derajat+=1.5)             // sudut awal 6.00, 0 derajat, bergerak 1.5 derajat setiap 5 menit
+       {
+       servo.write(derajat);                 
+       delay(300000);
+       }
+       for(derajat=176;derajat>=0;derajat-=5)
+       {
        servo.write(derajat);
-  
-                    
-  delay(500);}
-  delay(43200000);
-  }
-  
-  
+       delay(500);
+       }
+       delay(43200000);
+       }  
+        
+   else if  (waktu.mon=9 && waktu.hour=6 && waktu.min=0)       // September
+       {
+       delay(150);
+       servo.write(4);
+       for(derajat=4;derajat <= 176; derajat+=1)             // sudut awal 6.00, 0 derajat, bergerak 1 derajat setiap 4 menit
+       {
+       servo.write(derajat);                 
+       delay(240000);
+       }
+       for(derajat=176;derajat>=0;derajat-=5)
+       {
+       servo.write(derajat);
+       delay(500);
+       }
+       delay(43200000);
+       }  
+        
 }
